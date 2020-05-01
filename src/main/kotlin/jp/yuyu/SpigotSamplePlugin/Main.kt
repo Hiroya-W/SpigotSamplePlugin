0e03e8961f8f72e657977f5e554b07dc521a395f
Hiroya-W <e8239@g.maizuru-ct.ac.jp>
2020-05-01 23:44:11 +0900

    :sparkles: イベントの追加、HelloCommandの追加

--- src/main/kotlin/jp/yuyu/SpigotSamplePlugin/Main.kt
+++ src/main/kotlin/jp/yuyu/SpigotSamplePlugin/Main.kt
@@ -1,10 +1,12 @@
 package jp.yuyu.SpigotSamplePlugin
 
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
 
 class Main : JavaPlugin(){
     override fun onEnable() {
-        logger.info("Plugin is Running");
+        logger.info("Plugin is Running")
+        server.pluginManager.registerEvents(EventListener,this)
+        getCommand("hello")?.setExecutor(HelloCommand)
     }
-
 }
