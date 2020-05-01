679b9a79dbcf627798b45fc69f7fef3acd59f4a1
Hiroya-W <e8239@g.maizuru-ct.ac.jp>
2020-05-01 23:42:11 +0900

    :sparkles: HelloCommandを追加した

---
+++ src/main/kotlin/jp/yuyu/SpigotSamplePlugin/HelloCommand.kt
@@ -0,0 +1,13 @@
+package jp.yuyu.SpigotSamplePlugin
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+object HelloCommand : CommandExecutor{
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        sender.sendMessage("Hello ${sender.name}")
+        return true
+    }
+
+}
